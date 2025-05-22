74c56da4d2ade98f06e3ca0558b34cd781a81ab4
Subiksha K <subikshak23ads@srishakthi.ac.in>
2025-05-22 20:04:36 +0530

    Add files via upload

---
+++ day13.py
@@ -0,0 +1,10 @@
+report ={
+    "Water" : str(300)+'ml',
+    "Milk" : str(200)+'ml',
+    "coffee" : str(100)+'ml',
+    "Money" : "$"+str(0)
+
+}
+input = input("What would you like to want?(lattee,espresso,cappucino):  ")
+if input == 'report':
+    print(report)
